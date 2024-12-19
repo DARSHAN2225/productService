7a1c02b02a5d83111b93181670db7028ed977d45
Darshan <Darshanpartha07@yahoo.com>
2024-12-19 14:04:49 +0530

    DB connection and application.properties setup

--- src/main/java/dev/darshan/productservice/models/Product.java
+++ src/main/java/dev/darshan/productservice/models/Product.java
@@ -1,10 +1,13 @@
 package dev.darshan.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Entity
+@Builder
 public class Product extends BaseModel{
 
     private String title;
@@ -15,5 +18,9 @@ public class Product extends BaseModel{
 
     private double price;
 
+    // product and category
+    // 1:1
+    // m:1
+    @ManyToOne
     private Category category;
 }
